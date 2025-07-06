9acacf23bd5f77b432f62cb942ac35c50eeda4a4
bisma-bhatri92 <wajeehabhatti041@gmail.com>
2025-07-06 19:17:16 +0500

    Frontend Part 2 - Dashboard, Profile

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './pages/App';
+import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </BrowserRouter>
+);
